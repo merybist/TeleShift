1d08815fd04dbbc2bdeb042dd9ccbc114591be19
merybist <merybist@merybist.com>
2026-05-15 23:18:05 +0300

    feat: full localization and scheduler restoration

--- bot/states/forms.py
+++ bot/states/forms.py
@@ -2,3 +2,8 @@ from aiogram.fsm.state import StatesGroup, State
 
 class ConnectForm(StatesGroup):
     hash = State()
+
+class ScheduleForm(StatesGroup):
+    command = State()
+    time = State()
+    type = State()
